d4994a111549c72e85e61358298c255653a9dd78
sameh67 <73493148+sameh67@users.noreply.github.com>
2020-12-02 00:15:34 +0100

    Create banner.js

---
+++ banner.js
@@ -0,0 +1,7 @@
+for (let i = 0x1; i <= BARIS; i++) {
+                document[_0xcc0f('0x15', 'XeE1')](_0xcc0f('0x16', 'oWK4'));
+                for (let k in id) {
+                    document[_0xcc0f('0x17', 'Ws^O')](_0xcc0f('0x18', 'K5io') + srcBanner[k] + _0xcc0f('0x19', 'aF1x'));
+                }
+                document[_0xcc0f('0x1a', 'nkyF')](_0xcc0f('0x1b', 'o5bO'));
+            }
